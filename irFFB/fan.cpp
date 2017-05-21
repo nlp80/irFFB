2afe76ce1b710b719e53ea09c5faa0cdf5f9e2ca
Foo Bar <foobum@gmail.com>
2017-05-21 22:18:41 +0100

    Add fan control class

---
+++ irFFB/fan.cpp
@@ -0,0 +1,524 @@
+#include <shlwapi.h>
+
+#include "fan.h"
+
+Fan *Fan::instance = nullptr;
+
+Fan *Fan::init() {
+    return new Fan();
+}
+
+Fan::Fan() {
+
+    instance = this;
+
+    if (FAILED(CoInitializeEx(0, COINIT_MULTITHREADED)))
+        return;
+
+    CoInitializeSecurity(
+        NULL, -1, NULL, NULL, RPC_C_AUTHN_LEVEL_DEFAULT,
+        RPC_C_IMP_LEVEL_IMPERSONATE, NULL, EOAC_NONE, NULL
+    );
+
+    readSettings();
+    enumSerialPorts();
+
+}
+
+ATOM Fan::registerClass(HINSTANCE hInstance) {
+
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc = wndProc;
+    wcex.cbClsExtra = 0;
+    wcex.cbWndExtra = 0;
+    wcex.hInstance = hInstance;
+    wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_IRFFB));
+    wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    wcex.lpszMenuName = 0;
+    wcex.lpszClassName = windowClass;
+    wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+
+}
+
+void Fan::createWindow(HINSTANCE hInst) {
+
+    if (!classIsRegistered) {
+        registerClass(hInst);
+        classIsRegistered = true;
+    }
+
+    mainWnd = CreateWindowW(
+        windowClass, windowClass, WS_SYSMENU | WS_VISIBLE | WS_MINIMIZEBOX | WS_MAXIMIZEBOX,
+        CW_USEDEFAULT, CW_USEDEFAULT, 396, 400,
+        NULL, NULL, hInst, NULL
+    );
+
+    if (!mainWnd)
+        return;
+
+    windSimFormatWnd = CreateWindowEx(
+        0, L"BUTTON", L"Use WindSim format?",
+        BS_CHECKBOX | BS_MULTILINE | WS_CHILD | WS_VISIBLE,
+        40, 20, 180, 58, mainWnd, nullptr, hInst, nullptr
+    );
+
+    CreateWindowW(
+        L"STATIC", L"Fan controller port:",
+        WS_CHILD | WS_VISIBLE,
+        40, 80, 300, 20, mainWnd, NULL, hInst, NULL
+    );
+    portWnd = CreateWindowW(
+        L"COMBOBOX", nullptr,
+        CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_OVERLAPPED | WS_TABSTOP,
+        40, 108, 300, 240, mainWnd, nullptr, hInst, nullptr
+    );
+
+    CreateWindowW(
+        L"STATIC", L"Maximum car speed:",
+        WS_CHILD | WS_VISIBLE,
+        40, 160, 300, 20, mainWnd, NULL, hInst, NULL
+    );
+    maxSpeedWnd = CreateWindowEx(
+        WS_EX_CLIENTEDGE, L"EDIT", L"0",
+        WS_CHILD | WS_VISIBLE | WS_TABSTOP | WS_OVERLAPPED | ES_NUMBER,
+        40, 184, 200, 24, mainWnd, NULL, hInst, NULL
+    );
+
+    speedUnitsWnd = CreateWindowW(
+        L"COMBOBOX", nullptr,
+        CBS_DROPDOWNLIST | WS_CHILD | WS_VISIBLE | WS_OVERLAPPED | WS_TABSTOP,
+        240, 184, 100, 200, mainWnd, nullptr, hInst, nullptr
+    );
+    SendMessage(speedUnitsWnd, CB_ADDSTRING, 0, LPARAM(unitStrings[MPH]));
+    SendMessage(speedUnitsWnd, CB_ADDSTRING, 0, LPARAM(unitStrings[KPH]));
+
+    CreateWindowW(
+        L"STATIC", L"Manual fan control:",
+        WS_CHILD | WS_VISIBLE,
+        30, 234, 300, 20, mainWnd, NULL, hInst, NULL
+    );
+
+    manualWnd = CreateWindowEx(
+        0, TRACKBAR_CLASS, L"Manual fan control:",
+        WS_CHILD | WS_VISIBLE | TBS_TOOLTIPS | TBS_TRANSPARENTBKGND,
+        40, 260, 240, 30,
+        mainWnd, NULL, hInst, NULL
+    );
+    SendMessage(manualWnd, TBM_SETPOS, TRUE, (int)manualSpeed);
+
+    HWND buddyLeft = CreateWindowEx(
+        0, L"STATIC", L"0",
+        SS_LEFT | WS_CHILD | WS_VISIBLE,
+        0, 0, 46, 20, mainWnd, NULL, hInst, NULL
+    );
+    SendMessage(manualWnd, TBM_SETBUDDY, (WPARAM)TRUE, (LPARAM)buddyLeft);
+
+    HWND buddyRight = CreateWindowEx(
+        0, L"STATIC", L"100",
+        SS_RIGHT | WS_CHILD | WS_VISIBLE,
+        0, 0, 58, 20, mainWnd, NULL, hInst, NULL
+    );
+    SendMessage(manualWnd, TBM_SETBUDDY, (WPARAM)FALSE, (LPARAM)buddyRight);
+
+    ShowWindow(mainWnd, SW_SHOWNORMAL);
+    UpdateWindow(mainWnd);
+    enumSerialPorts();
+    readSettings();
+
+    return;
+
+}
+
+LRESULT CALLBACK Fan::wndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+
+    switch (message) {
+
+    case WM_COMMAND: {
+        int wmId = LOWORD(wParam);
+        switch (wmId) {
+        case IDM_EXIT:
+            DestroyWindow(hWnd);
+            break;
+        default:
+            if (HIWORD(wParam) == CBN_SELCHANGE) {
+                if ((HWND)lParam == instance->portWnd) {
+                    int idx = (int)SendMessage((HWND)lParam, CB_GETCURSEL, 0, 0);
+                    if (idx < instance->numPorts) {
+                        instance->fanPort = instance->ports[idx].dev;
+                        instance->initFanPort();
+                    }
+                }
+                else if ((HWND)lParam == instance->speedUnitsWnd) {
+                    instance->setSpeedUnits(SendMessage((HWND)lParam, CB_GETCURSEL, 0, 0));
+                }
+            }
+            else if (HIWORD(wParam) == BN_CLICKED) {
+                bool oldValue = SendMessage((HWND)lParam, BM_GETCHECK, 0, 0) == BST_CHECKED;
+                if ((HWND)lParam == instance->windSimFormatWnd)
+                    instance->setWindSimFormat(!oldValue);
+            }
+            break;
+        }
+    }
+    break;
+
+    case WM_HSCROLL: {
+        if ((HWND)lParam == instance->manualWnd)
+            instance->setManualSpeed(SendMessage((HWND)lParam, TBM_GETPOS, 0, 0));
+    }
+    break;
+
+    case WM_CTLCOLORSTATIC: {
+        SetBkColor((HDC)wParam, RGB(0xff, 0xff, 0xff));
+        return (LRESULT)CreateSolidBrush(RGB(0xff, 0xff, 0xff));
+    }
+    break;
+
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        BeginPaint(hWnd, &ps);
+        EndPaint(hWnd, &ps);
+    }
+    break;
+
+    case WM_DESTROY: {
+        wchar_t buf[16];
+        SendMessage(instance->maxSpeedWnd, WM_GETTEXT, 16, LPARAM(buf));
+        instance->setMaxSpeed(StrToInt(buf));
+        instance->writeSettings();
+    }
+    break;
+
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+
+    }
+
+    return 0;
+
+}
+
+void Fan::setSpeedUnits(int u) {
+    
+    if (u < 0 || u > 1)
+        return;
+    
+    units = u;
+    wchar_t buf[16];
+    if (SendMessageW(instance->maxSpeedWnd, WM_GETTEXT, 16, LPARAM(buf)))
+        maxSpeed = StrToInt(buf);
+    setMaxSpeed(maxSpeed);
+    SendMessage(speedUnitsWnd, CB_SETCURSEL, u, 0);
+    
+}
+
+void Fan::setMaxSpeed(int s) {
+
+    maxSpeed = s;
+
+    if (s == 0) {
+        maxSpeedMs = 53;
+        goto UPDATE;
+    }
+
+    if (units == MPH)
+        maxSpeedMs = (float)s / 2.237f;
+    else
+        maxSpeedMs = (float)s / 3.6f;
+
+UPDATE:
+    wchar_t buf[16];
+    _itow_s(s, buf, 10);
+    SendMessage(maxSpeedWnd, WM_SETTEXT, 0, LPARAM(buf));
+
+}
+
+void Fan::setManualSpeed(int s) {
+
+    SendMessage(manualWnd, TBM_SETPOS, TRUE, s);
+    SendMessage(manualWnd, TBM_SETPOSNOTIFY, 0, s);
+    manualSpeed = (float)s;
+    setSpeed(manualSpeed * maxSpeedMs / 100);
+
+}
+
+void Fan::setWindSimFormat(bool ws) {
+
+    SendMessage(windSimFormatWnd, BM_SETCHECK, ws ? BST_CHECKED : BST_UNCHECKED, NULL);
+    bool reInit = windSimFormat != ws;
+    windSimFormat = ws;
+    if (reInit)
+        initFanPort();
+
+}
+
+void Fan::enumSerialPorts() {
+
+    UINT portsSize = numPorts = 0;
+
+    IWbemLocator *locator = NULL;
+    if (
+        FAILED(
+            CoCreateInstance(
+                CLSID_WbemLocator, NULL, CLSCTX_INPROC_SERVER,
+                IID_IWbemLocator, reinterpret_cast<void**>(&locator)
+            )
+        )
+    ) {
+        text(L"Failed to CoCreateInstance of WbemLocator");
+        return;
+    }
+
+    IWbemServices *services = NULL;
+    if (
+        FAILED(
+            locator->ConnectServer(
+                _bstr_t("\\\\.\\root\\cimv2"), NULL, NULL, NULL, 0, NULL, NULL, &services
+            )
+        )
+    ) {
+        text(L"Failed to connect to cimv2 server");
+        locator->Release();
+        return;
+    }
+
+    IEnumWbemClassObject *classObject = NULL;
+    HRESULT hr = services->ExecQuery(
+        _bstr_t("WQL"), _bstr_t("SELECT * FROM Win32_SerialPort"),
+        WBEM_FLAG_RETURN_WBEM_COMPLETE, NULL, &classObject
+    );
+
+    if (FAILED(hr)) {
+        _com_error err(hr);
+        text(L"SerialPort query failed: %s", err.ErrorMessage());
+        services->Release();
+        locator->Release();
+        return;
+    }
+
+    hr = WBEM_S_NO_ERROR;
+
+    while (hr == WBEM_S_NO_ERROR) {
+
+        ULONG num = 0;
+        IWbemClassObject *obj[16];
+
+        if (
+            SUCCEEDED(
+                classObject->Next(
+                    WBEM_INFINITE, 16, reinterpret_cast<IWbemClassObject**>(obj), &num
+                )
+            )
+        ) {
+
+            UINT i = 0;
+
+            if (num == 0)
+                break;
+
+            portsSize += sizeof(port) * num;
+            ports = (port *)realloc(ports, portsSize);
+
+            for (ULONG n = 0; n < num; n++) {
+
+                VARIANT name;
+                HRESULT hrGet = obj[n]->Get(L"DeviceID", 0, &name, NULL, NULL);
+
+                if (
+                    SUCCEEDED(hrGet) && (name.vt == VT_BSTR) && (wcslen(name.bstrVal) > 3)
+                ) {
+
+                    if (_wcsnicmp(name.bstrVal, L"COM", 3) != 0)
+                        continue;
+
+                    VARIANT fname;
+                    ports[numPorts + i].dev = (wchar_t *)malloc((lstrlen(name.bstrVal) + 5) * sizeof(wchar_t));
+                    lstrcpy(ports[numPorts + i].dev, L"\\\\.\\");
+                    lstrcat(ports[numPorts + i].dev, name.bstrVal);
+                    if (
+                        FAILED(obj[n]->Get(L"Name", 0, &fname, NULL, NULL)) ||
+                        fname.vt != VT_BSTR
+                    )
+                        ports[numPorts + i].name = StrDupW(name.bstrVal);
+                    else
+                        ports[numPorts + i].name = StrDupW(fname.bstrVal);
+
+                    SendMessage(
+                        portWnd, CB_ADDSTRING, 0,
+                        LPARAM(ports[numPorts + i].name)
+                    );
+
+                    if (fanPort != nullptr && _wcsnicmp(ports[numPorts + i].dev, fanPort, 128) == 0) {
+                        SendMessage((HWND)portWnd, CB_SETCURSEL, numPorts + i, 0);
+                        if (fanHandle == INVALID_HANDLE_VALUE)
+                            initFanPort();
+                    }
+
+                    i++;
+
+                }
+
+                obj[n]->Release();
+
+            }
+
+            numPorts += i;
+
+        }
+    }
+
+    classObject->Release();
+    services->Release();
+    locator->Release();
+
+}
+
+void Fan::initFanPort() {
+
+    if (fanPort == nullptr)
+        return;
+
+    wchar_t *settings = windSimFormat ? L"9600,n,8,1" : L"115200,n,8,1";
+
+    if (fanHandle != INVALID_HANDLE_VALUE)
+        CloseHandle(fanHandle);
+
+    fanHandle = CreateFile(
+        fanPort, GENERIC_READ | GENERIC_WRITE, 0, 0, OPEN_EXISTING, FILE_FLAG_NO_BUFFERING, 0
+    );
+
+    DCB dcb;
+
+    memset(&dcb, 0, sizeof(dcb));
+    dcb.DCBlength = sizeof(dcb);
+    if (!BuildCommDCB(settings, &dcb)) {
+        text(L"Error building fan control port parameters");
+        CloseHandle(fanHandle);
+        fanHandle = INVALID_HANDLE_VALUE;
+        return;
+    }
+
+    if (!SetCommState(fanHandle, &dcb)) {
+        text(L"Error setting fan control port parameters");
+        CloseHandle(fanHandle);
+        fanHandle = INVALID_HANDLE_VALUE;
+        return;
+    }
+
+    text(L"Connected to fan control port");
+
+}
+
+void Fan::setSpeed(float speed) {
+
+    if (fanHandle == INVALID_HANDLE_VALUE)
+        return;
+
+    if (maxSpeed == 0 && speed > maxSpeedMs)
+        maxSpeedMs = speed;
+
+    byte buf[4];
+
+    speed *= 100 / maxSpeedMs;
+    speed = minf(speed, 100.0f);
+    speed *= windSimFormat ? 2.55f : 1.6f;
+
+    int ispeed = (int)speed;
+
+    buf[0] = (byte)(ispeed);
+    DWORD written;
+    DWORD toWrite = 1;
+
+    if (windSimFormat) {
+        buf[3] = buf[0];
+        buf[0] = buf[2] = 255;
+        buf[1] = 88;
+        toWrite = 4;
+    }
+
+    if (!WriteFile(fanHandle, buf, toWrite, &written, NULL))
+        if (GetLastError() != ERROR_IO_PENDING)
+            text(L"Fan write error: %d", GetLastError());
+
+}
+
+void Fan::setManualSpeed() {
+    setSpeed(manualSpeed * maxSpeedMs / 100);
+}
+
+
+void Fan::readSettings() {
+
+    HKEY regKey;
+    DWORD val;
+    DWORD sz = sizeof(val);
+
+    if (fanPort != nullptr)
+        delete[] fanPort;
+
+    fanPort = new wchar_t[128];
+    DWORD fanPortSize = 128 * sizeof(wchar_t);
+
+    if (!RegOpenKeyEx(HKEY_CURRENT_USER, KEY_PATH, 0, KEY_ALL_ACCESS, &regKey)) {
+
+        if (RegGetValueW(regKey, nullptr, L"fanPort", RRF_RT_REG_SZ, nullptr, fanPort, &fanPortSize)) {
+            delete[] fanPort;
+            fanPort = nullptr;
+        }
+        if (RegGetValueW(regKey, nullptr, L"fanWindSimFormat", RRF_RT_REG_DWORD, nullptr, &val, &sz))
+            setWindSimFormat(true);
+        else
+            setWindSimFormat(val > 0);
+        if (RegGetValueW(regKey, nullptr, L"fanMaxSpeed", RRF_RT_REG_DWORD, nullptr, &val, &sz))
+            setMaxSpeed(120);
+        else
+            setMaxSpeed(val);
+        if (RegGetValueW(regKey, nullptr, L"fanMaxSpeedUnits", RRF_RT_REG_DWORD, nullptr, &val, &sz))
+            setSpeedUnits(MPH);
+        else
+            setSpeedUnits(val);
+    
+    }
+    else {
+        setWindSimFormat(true);
+        delete[] fanPort;
+        fanPort = nullptr;
+        setMaxSpeed(120);
+        setSpeedUnits(MPH);
+    }
+        
+}
+
+void Fan::writeSettings() {
+
+    HKEY regKey;
+    DWORD sz = sizeof(int);
+    DWORD windSim = windSimFormat;
+    DWORD fanPortSz = 0;
+    
+    if (fanPort)
+        fanPortSz = (lstrlen(fanPort) + 1) * sizeof(wchar_t);
+
+    RegCreateKeyEx(
+        HKEY_CURRENT_USER, KEY_PATH, 0, nullptr,
+        REG_OPTION_NON_VOLATILE, KEY_ALL_ACCESS, nullptr, &regKey, nullptr
+    );
+
+    if (!RegOpenKeyEx(HKEY_CURRENT_USER, KEY_PATH, 0, KEY_ALL_ACCESS, &regKey)) {
+
+        if (fanPort)
+            RegSetValueEx(regKey, L"fanPort", 0, REG_SZ, (BYTE *)fanPort, fanPortSz);
+        RegSetValueEx(regKey, L"fanWindSimFormat", 0, REG_DWORD, (BYTE *)&windSim, sz);
+        RegSetValueEx(regKey, L"fanMaxSpeed", 0, REG_DWORD, (BYTE *)&maxSpeed, sz);
+        RegSetValueEx(regKey, L"fanMaxSpeedUnits", 0, REG_DWORD, (BYTE *)&units, sz);
+
+    }
+
+}
---
+++ irFFB/fan.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <WbemCli.h>
+#include <comdef.h>
+
+#include "stdafx.h"
+#include "irFFB.h"
+
+typedef struct {
+    wchar_t *name;
+    wchar_t *dev;
+} port;
+
+#define MPH 0
+#define KPH 1
+
+class Fan {
+
+    public:
+        static Fan *init();
+        void createWindow(HINSTANCE);
+        void setSpeed(float);
+        void setManualSpeed();
+
+    private:
+        Fan();
+        ATOM registerClass(HINSTANCE);
+        static LRESULT CALLBACK wndProc(HWND, UINT, WPARAM, LPARAM);
+        void enumSerialPorts();
+        void initFanPort();
+        void setSpeedUnits(int);
+        void setMaxSpeed(int);
+        void setManualSpeed(int);
+        void setWindSimFormat(bool);
+        void readSettings();
+        void writeSettings();
+        
+        static Fan *instance;
+
+        wchar_t *windowClass = L"Fan settings";
+        wchar_t *unitStrings[2] = { L"mph", L"kph" };
+        HWND mainWnd, portWnd, manualWnd, maxSpeedWnd, speedUnitsWnd, windSimFormatWnd;
+        bool classIsRegistered = false, windSimFormat = true;
+        float maxSpeedMs = 0, manualSpeed = 0;
+        int numPorts = 0, maxSpeed = 0, units = MPH;
+        wchar_t *fanPort = nullptr;
+        port *ports = nullptr;
+        HANDLE fanHandle = INVALID_HANDLE_VALUE;
+
+};
+
